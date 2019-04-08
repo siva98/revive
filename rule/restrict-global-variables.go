97e9668f427ff3854aafa3d0557d0f34ed34850a
siva98 <schokkapu1@sheffield.ac.uk>
2019-04-08 15:23:15 +0100

    add restrict-global-variables rule

---
+++ rule/restrict-global-variables.go
@@ -0,0 +1,68 @@
+package rule
+
+import (
+	"fmt"
+	"go/ast"
+
+	"github.com/mgechev/revive/lint"
+)
+
+type RestrictGlobalVariablesRule struct{}
+
+// Apply applies the rule to given file.
+func (r *RestrictGlobalVariablesRule) Apply(file *lint.File, _ lint.Arguments) []lint.Failure {
+	var failures []lint.Failure
+
+	fileAst := file.AST
+	walker := lintRestrictGlobalVariables{
+		file:    file,
+		fileAst: fileAst,
+		onFailure: func(failure lint.Failure) {
+			failures = append(failures, failure)
+		},
+	}
+
+	ast.Walk(walker, fileAst)
+
+	return failures
+}
+
+// Name returns the rule name.
+func (r *RestrictGlobalVariablesRule) Name() string {
+	return "restrict-global-variables"
+}
+
+type lintRestrictGlobalVariables struct {
+	file      *lint.File
+	fileAst   *ast.File
+	onFailure func(lint.Failure)
+}
+
+func (w lintRestrictGlobalVariables) Visit(node ast.Node) ast.Visitor {
+	for _, decl := range node.Decls {
+		gd, ok := decl.(*ast.GenDecl)
+		if !ok {
+			continue
+		}
+		for _, spec := range gd.Specs {
+			ast.Inspect(spec, func(n ast.Node) bool {
+				switch n := n.(type) {
+				case *ast.ValueSpec:
+					fmt.Println("Variable name: ")
+					for _, name := range n.Names {
+						fmt.Println(name.Name)
+						w.onFailure(lint.Failure{
+							Failure:    fmt.Sprintf("global variable detetced: %s, should not use global variable as they are not tracked on the ledger", name.Name),
+							Confidence: 1,
+							Node:       n,
+							Category:   "variable scope",
+						})
+						return w
+					}
+				}
+				return true
+			})
+		}
+	}
+	return w
+}
